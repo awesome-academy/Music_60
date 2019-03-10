e8bff27b88975b5af9437703329fc3bd54fef2ea
phuchh <koolkido1412@gmail.com>
2019-03-10 15:32:14 +0700

    create media player

---
+++ app/src/main/java/phuchh/com/music_60/mediaplayer/MediaRequest.java
@@ -0,0 +1,26 @@
+package phuchh.com.music_60.mediaplayer;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+@IntDef({
+        MediaRequest.LOADING,
+        MediaRequest.SUCCESS,
+        MediaRequest.UPDATE_PLAY_ACTIVITY,
+        MediaRequest.UPDATE_PLAYER,
+        MediaRequest.FAILURE,
+        MediaRequest.PAUSED,
+        MediaRequest.STOPPED
+})
+public @interface MediaRequest {
+    int LOADING = 1;
+    int SUCCESS = 2;
+    int UPDATE_PLAY_ACTIVITY = 3;
+    int UPDATE_PLAYER = 4;
+    int FAILURE = 5;
+    int PAUSED = 6;
+    int STOPPED = 7;
+}
---
+++ app/src/main/java/phuchh/com/music_60/mediaplayer/PlayMusic.java
@@ -0,0 +1,54 @@
+package phuchh.com.music_60.mediaplayer;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+public interface PlayMusic {
+    void create(int index);
+
+    void prepare();
+
+    void start();
+
+    void pause();
+
+    void stop();
+
+    int getDuration();
+
+    int getCurrentPosition();
+
+    boolean isPlaying();
+
+    void seek(int possition);
+
+    int getTrack();
+
+    void setTrack(int index);
+
+    void next();
+
+    void previous();
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({PlayerStatus.IDLE,
+            PlayerStatus.INITIALIZED,
+            PlayerStatus.PREPARING,
+            PlayerStatus.STARTED,
+            PlayerStatus.PAUSED,
+            PlayerStatus.STOPPED,
+            PlayerStatus.END,
+            PlayerStatus.PLAYBACK_COMPLETED})
+    @interface PlayerStatus {
+        int IDLE = 0;
+        int INITIALIZED = 1;
+        int PREPARING = 2;
+        int STARTED = 3;
+        int PAUSED = 4;
+        int STOPPED = 5;
+        int END = 6;
+        int PLAYBACK_COMPLETED = 7;
+    }
+}
